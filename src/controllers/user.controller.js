11f9c371809816c4a2f2ed7818ced96240cd1e73
ShreyashSalian <shreyashsalian15@gmail.com>
2024-03-16 16:53:21 +0530

    Adding the functionality in controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -448,6 +448,66 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
     }
 })
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    try{
+        const user = await User.aggregate(
+            [
+                {
+                    $match:{
+                        _id : new mongoose.Types.ObjectId(req.user?._id)
+                    }
+                },
+                {
+                    $lookup:{
+                        from:"videos",
+                        localField:"watchHistory",
+                        foreignField:"_id",
+                        as:"WatchedHistory",
+                        pipeline:[
+                            {
+                                $lookup:{
+                                    from:"users",
+                                    localField:"owner",
+                                    foreignField:"_id",
+                                    as:"videoOwnerDetails",
+                                    pipeline:[
+                                        {
+                                            $project:{
+                                                fullName:1,
+                                                username:1,
+                                                avatar:1,
+                                            }
+                                        }
+                                    ]
+                                }
+                            },
+                            {
+                                $addFields:{
+                                    owner:{
+                                        $first:"$owner"
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+
+            ]
+        );
+        if(!user?.length){
+            return new ApiError(400,"No history found");
+        }
+        return res.status(200).json(
+            new ApiResponse(200,user[0].WatchedHistory,"Watched History.")
+        )
+
+    }catch(err){
+        return new ApiError(500,err?.message || "Not ablw to get the watch history");   
+    }
+})
+
+
+
 export {
     registerUser,
     loginUser,
@@ -458,5 +518,6 @@ export {
     updateAccount,
     updateUserAvator,
     updateCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
